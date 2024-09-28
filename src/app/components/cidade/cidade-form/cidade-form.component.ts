c9de44cec3aaaa7dc95beb4fb445d563a5a4d512
GabrielMussatto <128410250+GabrielMussatto@users.noreply.github.com>
2024-09-28 08:44:47 -0300

    Modificando form-component de Cidade

--- src/app/components/cidade/cidade-form/cidade-form.component.ts
+++ src/app/components/cidade/cidade-form/cidade-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CidadeService } from '../../../services/cidade.service';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -9,46 +9,57 @@ import { MatInputModule } from '@angular/material/input';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
 import { Cidade } from '../../../models/cidade.model';
+import { Estado } from '../../../models/estado.model';
+import { EstadoService } from '../../../services/estado.service';
+import {MatSelectModule} from '@angular/material/select';
 
 @Component({
   selector: 'app-cidade-form',
   standalone: true,
-  imports: [NgIf, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatCardModule, MatToolbarModule],
+  imports: [NgIf, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatCardModule, MatToolbarModule, MatSelectModule],
   templateUrl: './cidade-form.component.html',
   styleUrl: './cidade-form.component.css'
 })
-export class CidadeFormComponent {
+export class CidadeFormComponent implements OnInit{
   formGroup: FormGroup;
+  estados: Estado[] = [];
 
   constructor(private formBuilder: FormBuilder,
     private cidadeService: CidadeService,
+    private estadoService: EstadoService,
     private router: Router,
     private activatedRoute: ActivatedRoute) {
 
+    //const cidade: Cidade = this.activatedRoute.snapshot.data['cidade'];
+
+    this.formGroup = this.formBuilder.group({
+      //id: [(cidade && cidade.id) ? cidade.id : null],
+      id: [null],
+      //nome: [(cidade && cidade.nome) ? cidade.nome : '', Validators.required],
+      nome: ['', Validators.required],
+      //idEstado: [(cidade && cidade.idEstado) ? cidade.idEstado : '', Validators.required]
+      estado: [null]
+    })
+  }
+  ngOnInit(): void {
+    this.estadoService.findAll().subscribe(data => {
+      this.estados = data;
+      this.initializeForm();
+    });
+  }
+
+  initializeForm(): void {
     const cidade: Cidade = this.activatedRoute.snapshot.data['cidade'];
 
+    // selecionando o estado
+    const estado = this.estados.find(estado => estado.id === (cidade?.estado?.id || null));
 
     this.formGroup = this.formBuilder.group({
       id: [(cidade && cidade.id) ? cidade.id : null],
       nome: [(cidade && cidade.nome) ? cidade.nome : '', Validators.required],
-      idEstado: [(cidade && cidade.idEstado) ? cidade.idEstado : '', Validators.required]
-    })
+      estado: [estado, Validators.required],
+    });
   }
-  /*
-    onSubmit() {
-      if (this.formGroup.valid) {
-        const novaCidade = this.formGroup.value;
-        this.cidadeService.insert(novaCidade).subscribe({
-          next: (cidadeCadastrada) => {
-            this.router.navigateByUrl('/cidades');
-          },
-          error: (err) => {
-            console.log('Erro ao salvar a cidade', + JSON.stringify(err));
-          }
-        })
-      }
-    }
-  */
 
   salvar() {
     if (this.formGroup.valid) {
@@ -79,7 +90,7 @@ export class CidadeFormComponent {
     if(this.formGroup.valid){
       const cidade = this.formGroup.value;
       if(cidade.id != null){
-        if(confirm("Deseja realmente excluir este Municipio? Não será possivel reverter.")){
+        if(confirm("Deseja realmente excluir esta cidade? Não será possivel reverter.")){
           this.cidadeService.delete(cidade).subscribe({
             next: () => {
               this.router.navigateByUrl('/cidades');
