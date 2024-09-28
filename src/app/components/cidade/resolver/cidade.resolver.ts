86c0c2f942a804cec4738cd2c66b9f8ee6d8892f
GabrielMussatto <128410250+GabrielMussatto@users.noreply.github.com>
2024-09-28 08:45:34 -0300

    Configurando resolver de Estado e Cidade

--- src/app/components/cidade/resolver/cidade.resolver.ts
+++ src/app/components/cidade/resolver/cidade.resolver.ts
@@ -1,7 +1,7 @@
 import { inject } from "@angular/core";
 import { ActivatedRouteSnapshot, ResolveFn, RouterStateSnapshot } from "@angular/router";
-import { Cidade } from "../../models/cidade.model";
-import { CidadeService } from "../../services/cidade.service";
+import { Cidade } from "../../../models/cidade.model";
+import { CidadeService } from "../../../services/cidade.service";
 
 
 export const cidadeResolver: ResolveFn<Cidade> = 
--- src/app/components/estado/resolver/estado.resolver.ts
+++ src/app/components/estado/resolver/estado.resolver.ts
@@ -1,7 +1,7 @@
 import { inject } from "@angular/core";
 import { ActivatedRouteSnapshot, ResolveFn, RouterStateSnapshot } from "@angular/router";
-import { Estado } from "../../models/estado.model";
-import { EstadoService } from "../../services/estado.service";
+import { Estado } from "../../../models/estado.model";
+import { EstadoService } from "../../../services/estado.service";
 
 
 export const estadoResolver: ResolveFn<Estado> = 
