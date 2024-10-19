3081e5539e97051dcb56e6b1765be81540f91a8c
GabrielMussatto <128410250+GabrielMussatto@users.noreply.github.com>
2024-10-19 10:52:10 -0300

    Adicionando component de paginação nas classes de estado

--- src/app/components/estado/estado-list/estado-list.component.ts
+++ src/app/components/estado/estado-list/estado-list.component.ts
@@ -7,26 +7,43 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTableModule } from '@angular/material/table';
 import { RouterModule } from '@angular/router';
+import { PageEvent } from '@angular/material/paginator';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-estado-list',
   standalone: true,
-  imports: [MatToolbarModule, MatIconModule, MatButtonModule, MatTableModule, RouterModule],
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule, MatTableModule, RouterModule, MatPaginatorModule],
   templateUrl: './estado-list.component.html',
   styleUrl: './estado-list.component.css'
 })
 export class EstadoListComponent implements OnInit {
-  displayedColumns: string[] = ['id', 'nome', 'sigla', 'acao'];
   estados: Estado[] = [];
+  displayedColumns: string[] = ['id', 'nome', 'sigla', 'acao'];
+
+  //variaveis de controle para a paginação
+  totalRecords = 0;
+  pageSize = 2;
+  page = 0;
 
   constructor(private estadoService: EstadoService) {
 
   }
 
   ngOnInit(): void {
-    this.estadoService.findAll().subscribe(
+    this.estadoService.findAll(this.page, this.pageSize).subscribe(
       data => { this.estados = data }
     );
+
+    this.estadoService.count().subscribe(
+      data => { this.totalRecords = data }
+    );
+  }
+
+  paginar(event: PageEvent): void {
+    this.page = event.pageIndex;
+    this.pageSize = event.pageSize;
+    this.ngOnInit();
   }
 
   excluir(estado: Estado): void {
